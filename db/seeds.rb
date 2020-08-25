780943cfdeb1ab24ee0f55edb17d7cbb63e20f57
Debora Ermel <debbyermel@gmail.com>
2020-08-24 17:31:39 -0700

    Add backend resources

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: 'Luke', password: 'password')
+User.create(username: 'Lea', password: 'password')
+User.create(username: 'Anakin', password: 'password')
+User.create(username: 'R2D2', password: 'password')
+User.create(username: 'Han Solo', password: 'password')
+User.create(username: 'C3PO', password: 'password')
