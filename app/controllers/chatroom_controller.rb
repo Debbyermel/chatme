d1b6e5cd4af433b7e06376c0512b338577ef7039
Debora Ermel <debbyermel@gmail.com>
2020-08-27 18:23:38 -0700

    Login form finish

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,6 +1,8 @@
 class ChatroomController < ApplicationController
+  before_action :required_user
 
   def index
+    @messages = Message.new
     @messages = Message.all
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get 'contact', to: 'pages#contact'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  post 'message', to: 'messages#create'
   get 'message', to: 'chatroom#index'
+  get 'contact', to: 'pages#contact'
 end
