dc347840972e1fee6bb51abce832914e04c705ce
Debora Ermel <debbyermel@gmail.com>
2020-08-27 17:04:28 -0700

    Login and flash msg

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'contact', to: 'pages#contact'
   get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   get 'message', to: 'chatroom#index'
 end
